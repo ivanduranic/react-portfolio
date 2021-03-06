47538855fb9511f78f059c85312fa1b74d943de1
Ivan Duranic <ivan.duranic@gmail.com>
2021-03-06 16:03:09 -0500

    added novelty project

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -29,7 +29,7 @@ function Portfolio() {
                 <div className="modal-content">
                   <div className="modal-header">
                     <h5 className="modal-title" id="exampleModalCenterTitle">
-                      Code Quiz
+                      Date Night Planner
                     </h5>
                     <button
                       type="button"
@@ -50,6 +50,73 @@ function Portfolio() {
                     <br />
                     <a
                       href="https://github.com/ivanduranic/date-night-planner"
+                      target="_blank"
+                    >
+                      <h4>Github repository</h4>
+                    </a>
+                  </div>
+                  <div className="modal-footer">
+                    <button
+                      type="button"
+                      className="btn btn-secondary"
+                      data-dismiss="modal"
+                    >
+                      Close
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* Novelty */}
+          <div className="pf">
+            <button
+              type="button"
+              data-toggle="modal"
+              data-target="#modalCenterWeather"
+            >
+              <img
+                src={require("../components/images/novelty.png")}
+                alt=""
+              />
+              <div className="pftitle">NOVELty</div>
+            </button>
+            <div
+              className="modal fade"
+              id="modalCenterWeather"
+              tabindex="-1"
+              role="dialog"
+              aria-labelledby="exampleModalCenterTitle"
+              aria-hidden="true"
+            >
+              <div
+                className="modal-dialog modal-dialog-centered"
+                role="document"
+              >
+                <div className="modal-content">
+                  <div className="modal-header">
+                    <h5 className="modal-title" id="exampleModalCenterTitle">
+                      NOVELty
+                    </h5>
+                    <button
+                      type="button"
+                      className="close"
+                      data-dismiss="modal"
+                      aria-label="Close"
+                    >
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  <div className="modal-body">
+                    <a
+                      href="https://novelty.herokuapp.com/"
+                      target="_blank"
+                    >
+                      <h4>Deployed App</h4>
+                    </a>
+                    <br />
+                    <a
+                      href="https://github.com/ivanduranic/NOVELty"
                       target="_blank"
                     >
                       <h4>Github repository</h4>
