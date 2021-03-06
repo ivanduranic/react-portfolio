a94d5ec90d10a65fe51e332b5f01f425743d93e6
Ivan Duranic <ivan.duranic@gmail.com>
2021-03-06 16:12:53 -0500

    added budget tracker

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -194,3 +194,68 @@ function Portfolio() {
               </div>
             </div>
           </div>
+
+          {/* Budget Tracker */}
+          <div className="pf">
+            <button
+              type="button"
+              data-toggle="modal"
+              data-target="#modalCenterNote"
+            >
+              <img src={require("../components/images/budgettracker.png")} alt="" />
+              <div className="pftitle">Budget Tracker</div>
+            </button>
+            <div
+              className="modal fade"
+              id="modalCenterNote"
+              tabindex="-1"
+              role="dialog"
+              aria-labelledby="exampleModalCenterTitle"
+              aria-hidden="true"
+            >
+              <div
+                className="modal-dialog modal-dialog-centered"
+                role="document"
+              >
+                <div className="modal-content">
+                  <div className="modal-header">
+                    <h5 className="modal-title" id="exampleModalCenterTitle">
+                    Budget Tracker
+                    </h5>
+                    <button
+                      type="button"
+                      className="close"
+                      data-dismiss="modal"
+                      aria-label="Close"
+                    >
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  <div className="modal-body">
+                    <a
+                      href="https://budget-trackerid.herokuapp.com"
+                      target="_blank"
+                    >
+                      <h4>Deployed App</h4>
+                    </a>
+                    <br />
+                    <a
+                      href="https://github.com/ivanduranic/budget-tracker"
+                      target="_blank"
+                    >
+                      <h4>Github repository</h4>
+                    </a>
+                  </div>
+                  <div className="modal-footer">
+                    <button
+                      type="button"
+                      className="btn btn-secondary"
+                      data-dismiss="modal"
+                    >
+                      Close
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
