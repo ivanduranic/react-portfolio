5f2f5965b13d1ef37dec3c2b207e4ed53c093cbf
Ivan Duranic <ivan.duranic@gmail.com>
2021-03-06 15:04:59 -0500

    setting structure in App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import AboutMe from "./components/AboutMe";
+import Contact from "./components/Contact";
+import Portfolio from "./components/Portfolio";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <div className="row d-flex flex-column flex-md-row px-md-4 bg-white border-bottom shadow-sm custom-height">
+          <h5 className="my-0 mr-md-auto font-weight-normal gree-box">
+            <Link to="/">Ivan Duranic</Link>
+          </h5>
+          <nav className="my-md-0 mr-md-3 navbar-stuff">
+            <Link className="p-2 text-dark" to="/">
+              About
+            </Link>
+            <Link className="p-2 text-dark" to="/portfolio">
+              Portfolio
+            </Link>
+            <Link className="p-2 text-dark" to="/contact">
+              Contact
+            </Link>
+          </nav>
+        </div>
+
+        
+        <Switch>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/portfolio">
+            <Portfolio />
+          </Route>
+          <Route path="/">
+            <AboutMe />
+          </Route>
+        </Switch>
+
+        <footer className="footer" id="footer">
+          copyright@
+        </footer>
+      </div>
+    </Router>
   );
 }
 
