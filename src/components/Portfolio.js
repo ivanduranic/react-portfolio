de99c7251c1e9f79139ed47be6947662942e3a6e
Ivan Duranic <ivan.duranic@gmail.com>
2021-03-06 16:10:06 -0500

    added esg

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -135,3 +135,62 @@ function Portfolio() {
               </div>
             </div>
           </div>
+
+          {/* Employee Summary Generator */}
+          <div className="pf">
+            <button
+              type="button"
+              data-toggle="modal"
+              data-target="#modalCenterTraveler"
+            >
+              <img src={require("../components/images/ESG.png")} alt="" />
+              
+              <div className="pftitle">Employee Summary Generator</div>
+            </button>
+            <div
+              className="modal fade"
+              id="modalCenterTraveler"
+              tabindex="-1"
+              role="dialog"
+              aria-labelledby="exampleModalCenterTitle"
+              aria-hidden="true"
+            >
+              <div
+                className="modal-dialog modal-dialog-centered"
+                role="document"
+              >
+                <div className="modal-content">
+                  <div className="modal-header">
+                    <h5 className="modal-title" id="exampleModalCenterTitle">
+                    Employee Summary Generator
+                    </h5>
+                    <button
+                      type="button"
+                      className="close"
+                      data-dismiss="modal"
+                      aria-label="Close"
+                    >
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  <div className="modal-body">
+                    <a
+                      href="https://github.com/ivanduranic/employee-summary-generator"
+                      target="_blank"
+                    >
+                      <h4>Github repository</h4>
+                    </a>
+                  </div>
+                  <div className="modal-footer">
+                    <button
+                      type="button"
+                      className="btn btn-secondary"
+                      data-dismiss="modal"
+                    >
+                      Close
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
